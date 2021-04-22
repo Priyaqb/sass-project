f1859a6c286e16d9b44e29e198dbb5816907f30d
Priya <priyas@qburst.com>
2021-04-22 11:49:47 +0530

    Removed unused plugin

--- webpack.config.js
+++ webpack.config.js
@@ -83,10 +83,7 @@ module.exports = (env, options) => ({
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery",
-      "window.jQuery": "jquery",
-      Popper: ["popper.js", "default"],
-      Util: "exports-loader?Util!bootstrap/js/dist/util",
-      Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+      "window.jQuery": "jquery"
     }),
   ],
   optimization: {
